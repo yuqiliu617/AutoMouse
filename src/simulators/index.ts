277dbd43e1382b3dfc69f18324e3979ffed36f2a
Yuqi Liu <yuqiliu617@outlook.com>
2024-04-15 00:33:03 -0700

    feat: add replay simulator

--- src/simulators/index.ts
+++ src/simulators/index.ts
@@ -1,5 +1,6 @@
 export type { MouseMotionSimulator, PointWithTimestamp } from "./common";
 
 export { default as bezierCurve } from "./bezierCurve";
+export { default as replay } from "./replay";
 export { default as uniformMotion } from "./uniformMotion";
 export { default as windMouse } from "./windMouse";
